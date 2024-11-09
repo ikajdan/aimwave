770a11fe608973d5373761a0cacb4552250bbd04
Ignacy Kajdan <ignacy.kajdan@gmail.com>
2024-11-09 18:23:58 +0100

    Spawn more targets on each wall

--- Assets/Scripts/TargetSpawner.cs
+++ Assets/Scripts/TargetSpawner.cs
@@ -9,40 +9,51 @@ public class TargetSpawner : MonoBehaviour
     private int score = 0;
 
     private readonly float wallHeight = 5.0f;
-    private readonly float tileSize = 5.0f;
+    private readonly float minTileSize = 2.0f;
+    private readonly float maxTileSize = 5.0f;
+    private readonly int tileCount = 3;
 
     public bool TargetSpawnerActive { get; set; } = false;
 
-    public void SpawnTargetsAtCenters()
+    public void SpawnTargets()
     {
         if (TargetSpawnerActive)
         {
-            SpawnTargetAtWall(wall1);
-            SpawnTargetAtWall(wall2);
-            SpawnTargetAtWall(wall3);
-            SpawnTargetAtWall(wall4);
+            SpawnTarget(wall1);
+            SpawnTarget(wall2);
+            SpawnTarget(wall3);
+            SpawnTarget(wall4);
         }
     }
 
-    void SpawnTargetAtWall(Transform wall)
+    void SpawnTarget(Transform wall)
     {
-        Vector3 randomPosition = GetRandomPositionAlongWall(wall);
-        Vector3 spawnPosition = randomPosition + new Vector3(0, wallHeight / 2, 0);
-        GameObject newTarget = Instantiate(targetPrefab, spawnPosition, Quaternion.identity);
-        newTarget.transform.rotation = Quaternion.LookRotation(wall.forward);
+        float totalLength = maxTileSize * tileCount;
+        float currentPosition = -totalLength / 2;
+
+        for (int i = 0; i < tileCount; i++)
+        {
+            float randomSpacing = Random.Range(minTileSize, maxTileSize);
+            currentPosition += randomSpacing;
+
+            Vector3 randomPosition = GetRandomPositionAlongWall(wall, currentPosition);
+            Vector3 spawnPosition = randomPosition + new Vector3(0, wallHeight / 2, 0);
+            GameObject newTarget = Instantiate(targetPrefab, spawnPosition, Quaternion.identity);
+            newTarget.transform.rotation = Quaternion.LookRotation(wall.forward);
+        }
     }
 
-    Vector3 GetRandomPositionAlongWall(Transform wall)
+    Vector3 GetRandomPositionAlongWall(Transform wall, float offset)
     {
         if (wall == wall3 || wall == wall4)
         {
-            float randomZ = Random.Range(wall.position.z - tileSize, wall.position.z + tileSize);
+            float randomZ = wall.position.z + offset;
             float randomX = wall.position.x;
             return new Vector3(randomX, wall.position.y, randomZ);
         }
         else
         {
-            float randomX = Random.Range(wall.position.x - tileSize, wall.position.x + tileSize);
+            float randomX = wall.position.x + offset;
             float randomZ = wall.position.z;
             return new Vector3(randomX, wall.position.y, randomZ);
         }
